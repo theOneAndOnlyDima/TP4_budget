195afada53009a0d7fa45d20a4547264195f3e8c
Dima <zlotea_dumitru@yahoo.com>
2023-11-01 13:36:26 -0400

    removed automatic reset

--- myscript.py
+++ myscript.py
@@ -5,4 +5,3 @@ goodHash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 
 os.system(f'git bisect start {badHash} {goodHash}')
 os.system('git bisect run python manage.py test')
-os.system('git bisect reset')
